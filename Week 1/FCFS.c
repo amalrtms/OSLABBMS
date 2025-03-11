94e351023b58450beb8876ff743750ee17d0f5a8
Amal Roy <82579762+amalrtms@users.noreply.github.com>
2025-03-11 12:30:11 +0530

    Add files via upload

---
+++ Week 1/FCFS.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+    int n, i, bt[20], wt[20], tat[20];
+    float wta = 0, tata = 0;
+    printf("Enter total no. of processes: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        printf("Enter Burst Time for Process %d: ", i+1);
+        scanf("%d", &bt[i]);
+    }
+    wt[0]=0;
+    tat[0]=bt[0];
+    for(i=1;i<n;i++){
+        wt[i]=wt[i-1]+bt[i - 1];
+        tat[i]=tat[i-1]+bt[i];
+        wta+=wt[i];
+        tata+=tat[i];
+    }
+    wta/=n;
+    tata/=n;
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for(i=0;i<n;i++){
+        printf("P%d\t%d\t\t%d\t\t%d\n",i+1,bt[i],wt[i],tat[i]);
+    }
+    printf("\nAverage Waiting Time = %.2f", wta);
+    printf("\nAverage Turnaround Time = %.2f", tata);
+}
